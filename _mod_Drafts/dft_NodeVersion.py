2150c1c8b6032a13907af7ce1805e57a586834b3
tianlunjiang <31866946+tianlunjiang@users.noreply.github.com>
2019-01-07 17:08:12 -0500

    Update dft_NodeVersion.py

--- _mod_Drafts/dft_NodeVersion.py
+++ _mod_Drafts/dft_NodeVersion.py
@@ -1,9 +1,17 @@
 '''
 
-- Find out env variabe: SHOW, SCENE, SHOT
-- Create directory: SHOW/SCENE/SHOT
+- Find out env variabe: USER, HOME, SHOW, SCENE, SHOTNAME or SHOT
+- Create directory: $HOME/.nuke/kuNodeVersion/SHOW/SCENE/SHOT
 
+# Save Node Version
 - name version: $NODENAME_nv$VERSION.nk
 - save selected node to the corrisponding dir
+
+# Load Node Version
+- select a node for loading version
+- find its corrisponding saved node base on NODENAME
+- prompt with NODENAME as title and nv$VERSION as menu item
+- combine selected menu item with NODENAME: NODENAME + nk$VERSION + .nk
+- incert the node
 
 '''
