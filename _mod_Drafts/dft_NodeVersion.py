a8fc7dba6acd7f1d1c72f0268e9f1af0e026bb31
tianlunjiang <31866946+tianlunjiang@users.noreply.github.com>
2019-01-07 14:42:54 -0500

    Create dft_NodeVersion.py

---
+++ _mod_Drafts/dft_NodeVersion.py
@@ -0,0 +1,9 @@
+'''
+
+- Find out env variabe: SHOW, SCENE, SHOT
+- Create directory: SHOW/SCENE/SHOT
+
+- name version: $NODENAME_nv$VERSION.nk
+- save selected node to the corrisponding dir
+
+'''
