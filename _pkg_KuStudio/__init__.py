ad04923bef7984ae95fbca9ca2dcf581f9899107
tianlunjiang <31866946+tianlunjiang@users.noreply.github.com>
2019-01-14 20:09:46 -0500

    Use eval() to import modules

--- _pkg_KuStudio/__init__.py
+++ _pkg_KuStudio/__init__.py
@@ -12,6 +12,13 @@ print "\n", "From Package: %s\n\nInstall:" % os.path.basename(dir)
 
 mods = [os.path.splitext(n)[0] for n in os.listdir(dir) if "mod_" in n]
 for m in mods:
-    print '---', m
+    
+    try:
+        pycmd = "from %s import *" % (m)
+        eval(pycmd)
+        
+        print '---', m
+    except:
+        print 'xxx', m
 
 print "\n...DONE\n"
