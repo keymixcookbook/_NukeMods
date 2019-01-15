7d5b3628c3a7304619b8ea7536e961d5038f7786
tianlunjiang <31866946+tianlunjiang@users.noreply.github.com>
2019-01-14 20:43:17 -0500

    Update dft_SelLayerContact.py

--- _mod_Drafts/dft_SelLayerContact.py
+++ _mod_Drafts/dft_SelLayerContact.py
@@ -3,10 +3,10 @@
 - Show the type of channels selected
 - ie. only "id", "spec", "multi", "multiSpecs", or "data"
 
-- Renderman Light: multi[1-9], diffMulti[0-9], specMulti[0-9]. id[0-10], diffCol, diffDir, diffInd, specDir, specInd, emission, subserface
+- Renderman Light: multi[0-9], diffMulti[0-9], specMulti[0-9]. id[0-10], diffCol, diffDir, diffInd, specDir, specInd, emission, subserface
 - Renderman Data: depth2, norm, position2, refPosition2, uv2, vel, shadow
 
-- Have Options to view: single layer OR group of layers with a given type[diffMulti, specMulti, diff, spec, shading, data]
+- Have Options to view: single layer OR group of layers with a given type[multiLights, diffMulti, specMulti, diff, spec, shading, data]
 - Only keep selected layer/layer groups
 - Output with LayerContactSheet
 
