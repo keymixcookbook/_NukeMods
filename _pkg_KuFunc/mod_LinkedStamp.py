a5e7d7066d8e73a30c5c93ad1b006f9491417f19
tianlunjiang <31866946+tianlunjiang@users.noreply.github.com>
2019-01-17 17:27:52 -0500

    added Button, Show Parent Node in DAG

--- _pkg_KuFunc/mod_LinkedStamp.py
+++ _pkg_KuFunc/mod_LinkedStamp.py
@@ -3,12 +3,13 @@ import nuke
 def LinkedStamp():
 
     '''
+    version 3.0
+        - added Button, Show Parent Node in DAG
+    
     version 2.1
-
         - added Python_button, connect to a node listed in Label
 
     version 2.0
-
         - added Callback, label changes when input changed
 
     '''
@@ -29,19 +30,30 @@ def LinkedStamp():
 
     # Add User knobs
 
-    py_cmd = "n=nuke.thisNode()\nn.setInput(0, nuke.toNode(n['label'].value()))"
+    py_cmd_restore= "n=nuke.thisNode()\nn.setInput(0, nuke.toNode(n['label'].value()))"
+    py_cmd_orig = "origNode = nuke.thisNode().input(0);\
+                    origXpos = origNode.xpos();\
+                    origYpos = origNode.ypos();\
+                    nuke.zoom(2, [origXpos,origYpos]);\
+                    nuke.thisNode()['selected'].setValue(False);\
+                    origNode['selected'].setValue(True);\
+                    nuke.show(origNode)"
 
 
     k_tab = nuke.Tab_Knob("LinkedStamp")
-    k_setInput = nuke.PyScript_Knob('link', "Set Input", py_cmd)
     k_text = nuke.Text_Knob('tx_nodename', "Set Input to: ")
+    k_setInput = nuke.PyScript_Knob('link', "Set Input", py_cmd_restore)
+    k_showParent = nuke.PyScript_Knob('orig', "Show Parent Node", py_cmd_orig)
+    k_showParent.clearFlag(nuke.STARTLINE)
 
     stp.addKnob(k_tab)
     stp.addKnob(k_text)
     stp.addKnob(k_setInput)
+    stp.addKnob(k_showParent)
 
-    k_setInput.setTooltip("Taking the node name from label and connect")
     k_text.setValue("<b>%s</b>" % (stp['label'].value()))
+    k_setInput.setTooltip("Taking the node name from label and connect")
+    k_showParent.setTooltip("Show parent node in DAG")
 
     def inputUpdate():
 
