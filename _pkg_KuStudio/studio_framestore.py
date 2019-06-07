b0c7474b9989e733a369ef90dda093c6826a2b86
TJ <31866946+tianlunjiang@users.noreply.github.com>
2019-06-07 16:17:42 -0400

    Create studio_framestore.py

---
+++ _pkg_KuStudio/studio_framestore.py
@@ -0,0 +1,15 @@
+def launchRV():
+    '''launch RV with multiple selections and stack'''
+    path_read = []
+
+    for n in nuke.selectedNodes('Group'):
+        if 'fAsset' in n.knobs():
+            with n:
+                for r in nuke.allNodes('Read'):
+                    path_read.append(nuke.filename(r))
+        else:
+            print "%s not a fAsset Read node" % n.name()
+
+    if path_read:
+        cmd = "rv -c -s 0.25 -over %s &" % ' '.join(path_read)
+        os.system(cmd)
