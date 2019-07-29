698eb28a12b5e093ef4423035f517468f9b6a527
TJ <31866946+tianlunjiang@users.noreply.github.com>
2019-07-29 11:55:23 -0400

    Update and rename menuItems.py to menu_items.py

--- menu_items.py
+++ menu_items.py
@@ -1,4 +1,10 @@
 import nuke
+from _pkg_KuFunc import *
+from _pkg_KuStudio import *
+from _mod_Download import *
+
+from _pkg_KuFunc.mod_KuUtility import *
+from _pkg_KuStudio.mod_KuStudio import *
 
 
 
