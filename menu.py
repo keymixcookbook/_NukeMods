1f4fe7e33086631080735131b48ed959f59cf55c
TJ <31866946+tianlunjiang@users.noreply.github.com>
2019-07-29 11:53:46 -0400

    Update menu.py

--- menu.py
+++ menu.py
@@ -15,12 +15,5 @@ to used in a VFX Studio enviroment
 
 
 
-from _pkg_KuFunc import *
-from _pkg_KuStudio import *
-from _mod_Download import *
-
-from _pkg_KuFunc.mod_KuUtility import *
-from _pkg_KuStudio.mod_KuStudio import *
-
 from menuItems import *
 from menuDefaults import *
