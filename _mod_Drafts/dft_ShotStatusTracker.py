ab4ce465ec11aea9b4625a9641ae583bf64d837b
Tianlun Jiang <jiangovfx@gmail.com>
2019-10-31 00:00:42 -0400

    start dft_ShotStatusTracker

---
+++ _mod_Drafts/dft_ShotStatusTracker.py
@@ -0,0 +1,10 @@
+import PySide.QtGui as QtGui
+import PySide.QtCore as QtCore
+import nuke, nukescripts
+
+
+
+class Core_ShotStatusTracker(QWidget):
+    def __init__(self):
+        super(Core_ShotStatusTracker, self).__init__()
+        
