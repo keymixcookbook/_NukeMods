4e0a852c42d67dd621b8f55c56afe4138f36dd70
Tianlun Jiang <jiangovfx@gmail.com>
2019-10-31 21:57:51 -0400

    update templet to fit Pyside panel

--- __Doc/_ScriptTemplets/qtApplications.py
+++ __Doc/_ScriptTemplets/qtApplications.py
@@ -1,20 +1,38 @@
-from PySide.QtGui import *
-from PySide.QtCore import *
-import sys
+import nuke, nukescripts
+try:
+    #nuke <11
+    import PySide.QtCore as QtCore
+    import PySide.QtGui as QtWidgets
+
+except:
+    #nuke>=11
+    import PySide2.QtCore as QtCore
+    import PySide2.QtGui as QtGui
+    import PySide2.QtWidgets as QtWidgets
 
 
-class PysidePanelApp(QWidget):
+
+
+class Core__PySidePanel_(QtWidgets.QWidget):
     def __init__(self):
-        super(PysidePanelApp, self).__init__()
+        super(_PySidePanel_, self).__init__()
 
 
+    def setDefault(self):
+        '''set default value when instancing'''
+
+
+    def run(self):
+        '''run panel instance'''
+        self.show()
 
 
 # Show the panel
-panel = PysidePanelApp()
+_PySidePanel_ = Core__PySidePanel_()
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    panel.show()
+    _PySidePanel_.run()
     app.exec_()
 else:
-    panel.show()
+    _PySidePanel_.run()
