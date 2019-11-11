3194700a64f19182811b430b5367123d3c7db45b
Tianlun Jiang <jiangovfx@gmail.com>
2019-11-11 00:05:12 -0500

    all basic feature functional

--- _mod_Drafts/dft_TimelineMarker.py
+++ _mod_Drafts/dft_TimelineMarker.py
@@ -2,6 +2,13 @@ from Qt import QtWidgets, QtGui, QtCore
 import sys, time, os, json
 
 
+
+
+########## SUPPORTING FUNCTION/CLASS ##########
+
+
+
+
 class MarkerButton(QtWidgets.QPushButton):
     def __init__(self, id, frame, label):
         super(MarkerButton, self).__init__()
@@ -10,7 +17,8 @@ class MarkerButton(QtWidgets.QPushButton):
         self.frame = frame
         self.label = label
 
-        self.resize(66,66)
+        self.setMinimumWidth(48)
+        self.setFixedHeight(48)
 
 
 class MarkerAdd(QtWidgets.QDialog):
@@ -18,13 +26,14 @@ class MarkerAdd(QtWidgets.QDialog):
     GUI for adding markers
     data to be stored:frame, tooltip, id(<SHOW>-<timestamp>, PHX-12345678)
     '''
-    def __init__(self, layout_obj):
+    def __init__(self, layout_obj, frame):
         super(MarkerAdd, self).__init__()
 
         # cur_frame = nuke.frame()
-        cur_frame = 1001
+        cur_frame = frame
         self.thisLayout = layout_obj
-        self.m_title = QtWidgets.QLabel('<b>Set Frame</b>')
+        self.m_title_frame = QtWidgets.QLabel('Frame: ')
+        self.m_title_label = QtWidgets.QLabel('Label: ')
         self.m_frame = QtWidgets.QSpinBox()
         self.m_frame.setMaximum(3000)
         self.m_frame.setPrefix('x')
@@ -35,16 +44,16 @@ class MarkerAdd(QtWidgets.QDialog):
         self.m_add = QtWidgets.QPushButton('Add Marker')
         self.m_add.clicked.connect(self.add)
 
-        self.layout_frame = QtWidgets.QHBoxLayout()
-        self.layout = QtWidgets.QVBoxLayout()
-        self.layout_frame.addWidget(self.m_title)
-        self.layout_frame.addWidget(self.m_frame)
-        self.layout.addLayout(self.layout_frame)
-        self.layout.addWidget(self.m_label)
-        self.layout.addWidget(self.m_add)
-        self.layout_frame.setSpacing(0)
+        self.layout = QtWidgets.QGridLayout()
+        self.layout.addWidget(self.m_title_frame, 0,0, QtCore.Qt.AlignRight)
+        self.layout.addWidget(self.m_frame, 0,1)
+        self.layout.addWidget(self.m_title_label, 1,0, QtCore.Qt.AlignRight)
+        self.layout.addWidget(self.m_label, 1,1)
+        self.layout.addWidget(self.m_add,2,0,1,2)
+        # self.layout.setSpacing(0)
         self.setLayout(self.layout)
         self.setWindowTitle("Add Frame Marker")
+        # self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
 
         self.m_label.setFocus()
         self.m_label.selectAll()
@@ -58,28 +67,25 @@ class MarkerAdd(QtWidgets.QDialog):
         thisMarker = MarkerButton(thisId, thisFrame, thisLabel)
         # thisMarker.customContextMenuRequested.connect(Core_TimelineMarker.removeMarker)
 
-        label_button = self.m_label.text() if len(self.m_label.text())<=10 else self.m_label.text()[:10]+'...'
+        label_button = self.m_label.text() if len(self.m_label.text())<=5 else self.m_label.text()[:5]+'...'
         thisMarker.setText(label_button)
         thisMarker.setToolTip( "<b>x%s:</b><br>%s<br>(id: %s)" % (thisMarker.frame, thisMarker.label, thisMarker.id) )
         self.thisLayout.addWidget(thisMarker)
         self.close()
 
-        #self.onSave()
 
 
-    def onSave(self):
-        '''save buttons when clicked'''
-        num_widget = layout_obj.count()
-        self.saveMarkers()
+
+########## MAIN FUNCTION/CLASS ##########
+
+
 
 
 class Core_TimelineMarker(QtWidgets.QWidget):
     def __init__(self):
         super(Core_TimelineMarker, self).__init__()
 
-        self.data_folder = "/Users/Tianlun/Desktop/_NukeTestScript"
-        self.data_filename = "TimelineMarker_dataset.json"
-        self.data_file = os.path.join(self.data_folder, self.data_filename)
+        self.data_file = self.getJSONPath()
 
         self.bt_add = QtWidgets.QPushButton('+')
         self.bt_add.clicked.connect(self.addMarker)
@@ -99,7 +105,6 @@ class Core_TimelineMarker(QtWidgets.QWidget):
         self.layout_editMarkers.setContentsMargins(0,0,0,0)
 
         self.layout_markers = QtWidgets.QHBoxLayout()
-        self.layout_markers.setSpacing(0)
         self.group_markers = QtWidgets.QGroupBox('Markers')
         self.group_markers.setContentsMargins(0,0,0,0)
         self.group_markers.setLayout(self.layout_markers)
@@ -128,6 +133,26 @@ class Core_TimelineMarker(QtWidgets.QWidget):
         self.reloadMarkers()
 
 
+    def getJSONPath(self):
+        '''
+        reutrn file path for the json file
+        Naming convension:
+        <HOME Dir>/.nuke/TimelineMarker/<SHOW>/<SHOT>_TMDataset.json
+        /Users/Tianlun/.nuke/TimelineMarker/PHX/str050_1010_TMDataset.json
+        '''
+        # Get pipline enviroment variables
+        data_SHOW = os.getenv('PL_SHOW') if os.getenv('PL_SHOW') else 'KUHQ'
+        data_SHOT = os.getenv('PL_SHOT') if os.getenv('PL_SHOT') else 'ku66_8686'
+
+        data_folder = os.path.join(os.getenv('HOME'), '.nuke','TimelineMarker')
+        data_filename = "%s_TMDataset.json" % data_SHOT
+        data_file = os.path.join(data_folder, data_SHOW, data_filename)
+
+        if not os.path.isdir(os.path.dirname(data_file)):
+            os.makedirs(os.path.dirname(data_file))
+
+        return data_file
+
 
     def saveMarkers(self):
         '''save marker into json files'''
@@ -150,9 +175,9 @@ class Core_TimelineMarker(QtWidgets.QWidget):
 
     def addMarker(self):
         '''add MarkerButton widget'''
-        self.p = MarkerAdd(self.layout_markers)
-        # add connect signal to the last widgets
+        self.p = MarkerAdd(self.layout_markers, 1001)
         self.p.exec_()
+        # add connect signal to the last widgets
         newWidget = self.layout_markers.itemAt(self.layout_markers.count()-1).widget()
         newWidget.clicked.connect(self.setFrame)
         newWidget.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
@@ -179,44 +204,51 @@ class Core_TimelineMarker(QtWidgets.QWidget):
 
     def reloadMarkers(self):
         '''reload from json file and rebuild MarkerButtons'''
-        # Clear Widgets
-        num_widgets = self.layout_markers.count()
-        all_widgets = [self.layout_markers.itemAt(n).widget() for n in range(num_widgets)]
-        for w in all_widgets:
-            self.layout_markers.removeWidget(w)
-            w.deleteLater()
-            print "Widget Removed: %s" % w.label
-
-
-
-        # Find data
         thisFile = self.data_file
-        thisData = []
-        with open(self.data_file, 'r') as f:
-            thisData = json.load(f)
-        print "loaded: %s" % thisFile
-        # Rebuild Widgets
-        for w in thisData:
-            thisFrame = w['frame']
-            thisId = w['id']
-            thisLabel = w['label']
-            thisMarker = MarkerButton(thisId, thisFrame, thisLabel)
 
-            label_button = thisLabel if len(thisLabel)<=10 else thisLabel[:10]+'...'
-            thisMarker.setText(label_button)
-            thisMarker.setToolTip( "<b>x%s:</b><br>%s<br>(id: %s)" % (thisMarker.frame, thisMarker.label, thisMarker.id) )
-            thisMarker.clicked.connect(self.setFrame)
-            thisMarker.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
-            thisMarker.customContextMenuRequested.connect(self.removeMarker_RClicked)
-            self.layout_markers.addWidget(thisMarker)
+        if not os.path.exists(thisFile):
+            print "No TMDataset.json file found"
+        else:
+            # Clear Widgets
+            num_widgets = self.layout_markers.count()
+            all_widgets = [self.layout_markers.itemAt(n).widget() for n in range(num_widgets)]
+            for w in all_widgets:
+                self.layout_markers.removeWidget(w)
+                w.deleteLater()
+                print "Markers Removed: %s" % w.label
 
-        print "final widget count: %s" % self.layout_markers.count()
+            # Find data
+            thisData = []
+            with open(self.data_file, 'r') as f:
+                thisData = json.load(f)
+            print "loaded: %s" % thisFile
+            # Rebuild Widgets
+            for w in thisData:
+                thisFrame = w['frame']
+                thisId = w['id']
+                thisLabel = w['label']
+                thisMarker = MarkerButton(thisId, thisFrame, thisLabel)
 
+                label_button = thisLabel if len(thisLabel)<=5 else thisLabel[:5]+'...'
+                thisMarker.setText(label_button)
+                thisMarker.setToolTip( "<b>x%s:</b><br>%s<br>(id: %s)" % (thisMarker.frame, thisMarker.label, thisMarker.id) )
+                thisMarker.clicked.connect(self.setFrame)
+                thisMarker.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+                thisMarker.customContextMenuRequested.connect(self.removeMarker_RClicked)
+                self.layout_markers.addWidget(thisMarker)
 
+            self.group_markers.setTitle('SHOT: ' + os.path.basename(self.data_file).split('_TMDataset.json')[0])
+            print "Markers added: %s" % self.layout_markers.count()
 
 
     def loadFromFile(self):
         '''load buttons from a json file'''
+        sel_json = QtWidgets.QFileDialog().getOpenFileName(self,"Select a TMDataset json file", os.path.join(os.getenv('HOME'), '.nuke'), 'JSON files (*.json)')[0]
+        if sel_json:
+            self.data_file = sel_json
+            self.reloadMarkers()
+        else:
+            print "Inviald file path"
 
 
     def setFrame(self):
