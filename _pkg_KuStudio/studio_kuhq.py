afc54eb80c8f154ddca15962d296254e55847e38
Tianlun Jiang <jiangovfx@gmail.com>
2020-03-01 17:51:06 -0800

    pipline stuff

--- _pkg_KuStudio/studio_kuhq.py
+++ _pkg_KuStudio/studio_kuhq.py
@@ -1,10 +1,13 @@
 import nuke
 import nukescripts
 import os
+from Qt import QtGui, QtWidgets, QtCore
 
 
 mu = nuke.menu('Nuke').addMenu('KUHQ')
 mu.addCommand('Sequence Loader', 'studio_kuhq.SequenceLoader()')
+mu.addCommand('Write', 'studio_kuhq.kuWrite()','w')
+mu.addCommand('Read from Write', 'studio_kuhq.readFromWrite()','alt+w')
 
 
 
@@ -101,3 +104,116 @@ def SequenceLoader():
             aov_beauty = nuke.toNode(RGBA)
             aov_rest = [n for n in nuke.allNodes('Read') if n != aov_beauty]
             shuffling(aov_beauty, aov_rest)
+
+
+
+
+def kuWrite():
+    '''Adding inputs for auto generate output path'''
+
+    RENDER_TYPE = ['comp', 'precomp']
+
+    def set_settings(node):
+        '''setting export element settings'''
+        PADDING_VER = 3
+        PADDING_FRAME = '%04d'
+        projDir = node['fp_proj'].value()
+        scene = node['tx_scene'].value()
+        out_type = node['mu_type'].value()
+        ver = int(node['nm_ver'].value())
+        out_file = '%s_%s_v%s' % (scene, out_type, str(ver).zfill(PADDING_VER))
+        out_path = os.path.join(projDir,'EXPORT', scene, out_file, '%s.%s.exr' % (out_file, PADDING_FRAME))
+        node['file'].setValue(out_path)
+        ch = 'rgb' if out_type == 'comp' else 'all'
+        node['channels'].setValue(ch)
+
+        print "==========\n\n%s\n\n%s-%s\n\n==========" % (
+            out_file, nuke.Root()['first_frame'].value(), nuke.Root()['last_frame'].value()
+            )
+
+
+
+    def render_node(node):
+        '''launch render'''
+        out_path = node['file'].value()
+        askMessage = "Render Node: %s\nFile: %s\nFramerage: %s-%s\n" % (
+            node.name(), os.path.basename(node['file'].value()), nuke.Root()['first_frame'].value(), nuke.Root()['last_frame'].value()
+            )
+        c = nuke.ask(askMessage)
+        if c:
+            if os.path.exists(out_path):
+                nuke.execute(node)
+            else:
+                os.makedirs(out_path)
+                nuke.execute(node)
+        else:
+            print "user cancelled"
+
+
+
+    node = nuke.createNode('Write')
+    node.setName('KuWrite')
+    node.knob('file').setEnabled(False)
+
+    k_pipline = nuke.Text_Knob('kupipline', 'kuWrite', 'kuWrite') # Ku Pipline Identifier
+
+    k_tab = nuke.Tab_Knob('tb_kuWrite', 'kuWrite')
+    k_projDir = nuke.File_Knob('fp_proj', 'proj dir')
+    k_scene = nuke.String_Knob('tx_scene', 'scene', os.path.basename(nuke.scriptName()).split('_v')[0])
+    k_type = nuke.Enumeration_Knob('mu_type', 'type', RENDER_TYPE)
+    k_ver = nuke.Int_Knob('nm_ver', 'version', 1)
+    k_div = nuke.Text_Knob('divider','')
+    k_set = nuke.PyScript_Knob('bt_set', '<b>Set Write</b>', 'studio_kuhq.kuWrite.set_setting(nuke.thisNode())')
+    k_render = nuke.PyScript_Knob('bt_render', '<b>Render</b>', 'studio_kuhq.kuWrite.render_node(nuke.thisNode())')
+
+    path_proj = os.path.dirname(nuke.script_directory())
+    k_projDir.setValue(path_proj)
+    k_ver.setValue(1)
+    k_render.clearFlag(nuke.STARTLINE)
+    k_pipline.setVisible(False)
+
+    for k in [k_tab, k_pipline, k_projDir, k_scene, k_type, k_ver, k_div, k_set, k_render]:
+        node.addKnob(k)
+
+    set_kuWrite(node)
+
+
+
+def readFromWrite():
+    '''create read from write'''
+    for n in nuke.selectedNodes('Read'):
+        path = os.path.dirname(n['file'].value())
+        exrs = nuke.getFileNameList(path)[0] # ['albedo_1k_v001.####.exr 1-96']
+        frames, range = exrs.split(' ')
+        first, last = range.split('-')
+        first = int(first)
+        last = int(last)
+        nuke.nodes.Read(
+            file=os.path.join(path, frames),
+            name=os.path.basename(path),
+            first=first,
+            last=last,
+            label=os.path.basename(path)
+            )
+
+
+
+
+
+
+########### NUKE KNOB CHANGED ###########
+
+
+
+
+def ku_knobChange():
+    '''knob change function to call'''
+
+    n = nuke.thisNode()
+    k = nuke.thisKnob()
+
+    if n['kupipline'].value() == 'kuWrite':
+        ku_nukePipline.kuWrite.set_kuWrite(n)
+
+
+nuke.addKnobChanged(ku_knobChange, nodeClass='Write')
