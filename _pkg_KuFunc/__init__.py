ff73d48f3bbdaf9dae423ae4502a9f4b77f24222
TianlunPC <johnsonjtl@hotmail.com>
2021-04-14 23:57:49 -0700

    py3

--- _pkg_KuFunc/__init__.py
+++ _pkg_KuFunc/__init__.py
@@ -8,7 +8,7 @@ __init__.py for _pkg_KuFunc
 import os
 
 dir = os.path.dirname(__file__)
-print "\n", "From Package: %s\n\nInstall:" % os.path.basename(dir)
+print( "\n From Package: %s\n\nInstall:" % os.path.basename(dir))
 
 mods = [os.path.splitext(n)[0] for n in os.listdir(dir) if 'mod_' in n and '.pyc' not in n and 'upt_' not in n]
 __all__ = []
@@ -16,6 +16,6 @@ __all__ = []
 for m in mods:
 
     __all__.append(m)
-    print '---', m
+    print('---'), m
 
-print "\n...DONE\n"
+print("\n...DONE\n")
