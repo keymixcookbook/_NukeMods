6ced6dc3b5ddf0402c17d41551fb94df3e7fba2d
Tianlun Jiang <jiangovfx@gmail.com>
2021-04-28 22:41:42 -0700

    directory structure optimize

--- __Appendix/_nukeSystemScripts/_nuke.system/localization.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""
-APIs for Nuke's localization functionality.
-"""
-
-# Import the internal module from C++
-from _nuke_localization import *
--- __Appendix/_nukeSystemScripts/_nukescripts.system/stereo.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2010 The Foundry Visionmongers Ltd.  All Rights Reserved.
-
-import nuke
-
-def setViewsForStereo():
-  nuke.root().knob('views').fromScript( '\n'.join( ('left #ff0000', 'right #00ff00') ) )
-
