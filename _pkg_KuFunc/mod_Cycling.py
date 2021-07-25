eaca3fd7bd366e854ed51f14ae0293d06ee45da9
TianlunPC <johnsonjtl@hotmail.com>
2021-07-24 20:57:06 -0700

    Cycling: merge ouput to knobChanged, add prev mode

--- _pkg_KuFunc/mod_Cycling.py
+++ _pkg_KuFunc/mod_Cycling.py
@@ -29,7 +29,7 @@ import nuke, nukescripts
 
 
 
-__VERSION__		= '1.0'
+__VERSION__		= '2.0'
 __OS__			= platform.system()
 __AUTHOR__	 	= "Tianlun Jiang"
 __WEBSITE__		= "jiangovfx.com"
@@ -41,6 +41,10 @@ __TITLE__		= "Cycling v%s" % __VERSION__
 
 def _version_():
 	ver='''
+
+	version 2.0
+	- merge knobChange for output channels
+	- add prev mode
 
 	version 1.0
 	- one hotkey cycling channels and merge operations
@@ -68,12 +72,7 @@ def Cycling(mode='regular'):
 		nuke.message('Please select a node')
 	else:
 		if n.Class()=='Merge2':
-			rgbOnly = ['plus', 'minus', 'multiply', 'hypot']
 			coreCycle('op',n,mode)
-			if n['operation'].value() in rgbOnly:
-				n['output'].setValue('rgb')
-			else:
-				n['output'].setValue('rgba')
 		else:
 			coreCycle('ch',n,mode)
 
@@ -98,14 +97,21 @@ def setKnob(node, knob, ls_k):
 		k_new = None
 		if k_cur in ls_k:
 			k_count = ls_k.index(k_cur)
-			k_new_idx = 0 if k_count==len(ls_k)-1 else k_count+1
+			if k_count==len(ls_k)-1:
+				k_new_idx = 0
+			elif k_count < 0:
+				k_new_index = len(ls_k)
+			elif mode == 'regular':
+				k_new_index = k_count+1
+			elif mode == 'prev'
+				k_new_index = k_count-1
 			k_new = ls_k[int(k_new_idx)]
 		else:
 			k_new = ls_k[0]
 		k.setValue(k_new)
-		print "%s set to %s" % (node.name(), k_new)
+		print("%s set to %s" % (node.name(), k_new))
 	except:
-		print "knob %s not in %s" % (knob, node.name())
+		print("knob %s not in %s" % (knob, node.name()))
 
 
 def coreCycle(knob, node, mode):
@@ -114,6 +120,7 @@ def coreCycle(knob, node, mode):
 	@knob='op': operation for merge node
 	@mode='regular': often used channels/operations in merge node
 	@mode='all': all channels/operations in last selected node
+	@mode='prev': switch to previous operation in list
 	@node: input node, obj
 	'''
 	if knob=='ch':
@@ -133,9 +140,57 @@ def coreCycle(knob, node, mode):
 			setKnob(node,'channels', ls_ch)
 	elif knob=='op':
 		ls_op = []
-		if mode == 'regular':
-			ls_op = ['plus', 'minus', 'mask', 'stencil', 'over', 'under', 'max', 'min', 'hypot']
+		if mode in ['regular', 'prev']:
+			ls_op = ['plus', 'minus', 'mask', 'stencil', 'over', 'under', 'max', 'min', 'multiply', 'divide', 'hypot']
 		elif mode == 'all':
 			ls_op = node['operation'].values()
 		setKnob(node, 'operation', ls_op)
 
+
+
+
+
+
+
+#-------------------------------------------------------------------------------
+#-Callback Functions
+#-------------------------------------------------------------------------------
+
+
+
+
+def ch_merge_out():
+	"""channel output for merge node"""
+	n = nuke.thisNode()
+	k = nuke.thisKnob()
+	
+	rgbOnly = [
+		'plus', 'minus', 'multiply', 'hypot', 
+		'color-dodge',  'color-burn', 'average',
+		'geometric', 'overlay', 'soft-light',
+		]
+	
+	if k:
+		if k.name() == 'operation':
+			if k.value() in rgbOnly:
+				n['output'].setValue('rgb')
+			else:
+				n['output'].setValue('rgba')
+	else:
+		if n['operation'].value() in rgbOnly:
+			n['output'].setValue('rgb')
+		else:
+			n['output'].setValue('rgba')
+
+
+
+
+
+#-------------------------------------------------------------------------------
+#-Adding Callbacks
+#-------------------------------------------------------------------------------
+
+
+
+
+nuke.addKnobChanged(ch_merge_out, nodeClass='Merge2')
